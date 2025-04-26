0c9162e1b4b3f73e8798527ef57d976ad081d331
Sharllyrose Wanjala <sharllyrose100@gmail.com>
2025-04-26 08:42:36 +0300

    Update server.js to use ESM syntax for json-server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
+
 const server = jsonServer.create();
 const router = jsonServer.router('src/db.json');
 const middlewares = jsonServer.defaults();
